476a3b09af67f3e12ac2242aae3e02c847903701
Pete Gautam <2481471g@student.gla.ac.uk>
2021-01-23 15:34:19 +0000

    Completed Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -15,43 +15,51 @@ def populate():
     python_pages = [
         {
             'title': 'Official Python Tutorial',
-            'url':'http://docs.python.org/3/tutorial/'
+            'url':'http://docs.python.org/3/tutorial/',
+            'views':708
         }, {
             'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'
+            'url':'http://www.greenteapress.com/thinkpython/',
+            'views':429
         }, {
             'title':'Learn Python in 10 Minutes',
-            'url':'http://www.korokithakis.net/tutorials/python/'
+            'url':'http://www.korokithakis.net/tutorials/python/',
+            'views':234
         } 
     ]
 
     django_pages = [
         {
             'title':'Official Django Tutorial',
-            'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'
+            'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views':2511
         }, {
             'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'
+            'url':'http://www.djangorocks.com/',
+            'views':614
         }, {
             'title':'How to Tango with Django',
-            'url':'http://www.tangowithdjango.com/'
+            'url':'http://www.tangowithdjango.com/',
+            'views':274
         } 
     ]
 
     other_pages = [
         {
             'title':'Bottle',
-            'url':'http://bottlepy.org/docs/dev/'
+            'url':'http://bottlepy.org/docs/dev/',
+            'views':340
         }, {
             'title':'Flask',
-            'url':'http://flask.pocoo.org'
+            'url':'http://flask.pocoo.org',
+            'views':181
         } 
     ]
 
     cats = {
-        'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
-        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} 
+        'Python': {'pages': python_pages, 'views': 128, 'likes': 64, 'slug': 'python'},
+        'Django': {'pages': django_pages, 'views': 64, 'likes': 32, 'slug': 'django'},
+        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16, 'slug': 'other-frameworks'} 
     }
 
     # If you want to add more categories or pages,
@@ -60,14 +68,14 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        c = add_cat(cat, cat_data['views'], cat_data['likes'], cat_data['slug'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
-    # # Print out the categories we have added.
-    # for c in Category.objects.all():
-    #     for p in Page.objects.filter(category=c):
-    #         print(f'- {c}: {p}')
+    # Print out the categories we have added.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
@@ -76,12 +84,11 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name, views, likes):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+def add_cat(name, views, likes, slug):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes, slug=slug)[0]
     c.save()
     return c
 
-# Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
-    path('rango/', include('rango.urls')),
+    path('admin/', admin.site.urls, name="admin"),
+    path('rango/', include('rango.urls'), name="rango"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
