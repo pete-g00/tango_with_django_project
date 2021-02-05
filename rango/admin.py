02c08f6f82da2fde05679f09180795ad040c4ad2
Pete Gautam <2481471g@student.gla.ac.uk>
2021-02-05 15:10:37 +0000

    Re-uploaded Chapter 4 without the test file

--- rango/admin.py
+++ rango/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from rango.models import Category, Page
 
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Category, CategoryAdmin)
-
-class PageAdmin(admin.ModelAdmin):
-    fields = ['category', 'title', 'url', 'views']
-    list_display = ('title', 'category', 'url')
-
-admin.site.register(Page, PageAdmin)
+# Register your models here.
--- rango/urls.py
+++ rango/urls.py
@@ -8,5 +8,4 @@ app_name = "rango"
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug>/', views.show_category, name="show_category"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
